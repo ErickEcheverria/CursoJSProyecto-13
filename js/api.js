7b16a5dfb873fedc8d9f704b62c7a17887e4d1fa
Erick Echeverría <erickecheverria66@gmail.com>
2021-07-12 18:41:00 -0600

    Correcion

--- js/api.js
+++ js/api.js
@@ -42,23 +42,23 @@ class API {
 }
 
 function mostrarSpinner(){
+     while(UI.divResultado.firstChild){
+          UI.divResultado.removeChild(UI.divResultado.firstChild);
+     }
  
      const spinner = document.createElement('div');
      spinner.classList.add('spinner');
  
      spinner.innerHTML = `
-     <div class="sk-cube sk-cube1"></div>
-     <div class="sk-cube sk-cube2"></div>
-     <div class="sk-cube sk-cube3"></div>
-     <div class="sk-cube sk-cube4"></div>
-     <div class="sk-cube sk-cube5"></div>
-     <div class="sk-cube sk-cube6"></div>
-     <div class="sk-cube sk-cube7"></div>
-     <div class="sk-cube sk-cube8"></div>
-     <div class="sk-cube sk-cube9"></div>
+         <div class="dot1"></div>
+         <div class="dot2"></div>
      `;
  
      UI.divResultado.appendChild(spinner);
+
+     setTimeout(() => {
+          UI.divResultado.removeChild(UI.divResultado.firstChild);
+     }, 3000);
  }
 
 export default API;
